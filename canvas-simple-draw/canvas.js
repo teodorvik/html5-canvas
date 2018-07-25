76ade21396bace39052570b1a021fab26fa8beea
Teodor Vik <teodor.johan.vik@gmail.com>
2018-07-25 21:04:07 +0200

    Drawing text

--- canvas-simple-draw/canvas.js
+++ canvas-simple-draw/canvas.js
@@ -20,11 +20,17 @@ window.addEventListener('resize', resizeCanvas);
  * Random sized square at centered at x,y 
  */
 const createSquare = (x,y) => {
-    const rand = Math.random();
-    const size = 100*rand;
-    c.fillRect(x-size*.5,y-size*.5,size,size);
+    const randSize = Math.random() * 100;
+    c.fillRect(x-randSize*.5,y-randSize*.5,randSize,randSize);
+};
+
+const createText = (x,y) => {
+    const str = 'canvas-simple-draw';
+    const txt = c.measureText(str);
+    c.fillText(str, x-txt.width*.5, y);
 };
 
 canvas.addEventListener('click', (e) => {
-    createSquare(e.clientX, e.clientY);
+    const rand = Math.floor(Math.random() * 2);
+    rand !== 0 ? createSquare(e.clientX, e.clientY) : createText(e.clientX, e.clientY);
 });
